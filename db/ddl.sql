7468ef7332af5e553bec2ef6860cf05ccca6b4dd
dudumiaoer <1436607900@qq.com>
2019-11-12 14:57:50 +0800

    init

---
+++ db/ddl.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `sys_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY(`id`)
+)  ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `sys_user` VALUES ('1', 'admin', '123');
+INSERT INTO `sys_user` VALUES ('2', 'jitwxs', '123');
